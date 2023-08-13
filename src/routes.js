230f9c4295bd10b0bca6ac93d431eabb945baa84
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-08-13 20:15:35 -0300

    feat: declaring router on rountes.js

--- src/routes.js
+++ src/routes.js
@@ -2,3 +2,4 @@ const express = require('express');
 const axios = require('axios');
 const { locationValidation, removeAccents, translatedConditions } = require('./utils.js');
 
+const router = express.Router();
