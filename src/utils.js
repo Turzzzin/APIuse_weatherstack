fc9301e61c34701274b18a8df752744fc7cc2a1a
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-08-13 20:11:18 -0300

    feat: add translatedConditions + module.exports on util.js

--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,40 @@ function locationValidation(location) {
 function removeAccents(location) {
     return location.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 };
+
+// Mapeamento para tradução da 'descrição' do Tempo
+const translatedConditions = {
+    'Clear': 'Céu limpo',
+    'Partly cloudy': 'Parcialmente nublado',
+    'Overcast': 'Nublado',
+    'Mist': 'Neblina',
+    'Patchy rain possible': 'Possibilidade de chuva',
+    'Rain': 'Chuva',
+    'Drizzle': 'Chuvisco',
+    'Showers': 'Chuvas',
+    'Heavy rain': 'Chuva intensa',
+    'Light rain': 'Chuva fraca',
+    'Thunderstorms': 'Tempestades',
+    'Thunderstorm with rain': 'Temporais com chuva',
+    'Thunderstorm with heavy rain': 'Temporais com chuva intensa',
+    'Thunderstorm with light rain': 'Temporais com chuva fraca',
+    'Snow': 'Neve',
+    'Blizzard': 'Nevasca',
+    'Fog': 'Nevoeiro',
+    'Hail': 'Granizo',
+    'Sleet': 'Aguaceiros',
+    'Tornado': 'Tornado',
+    'Sunny': 'Ensolarado',
+    'Mostly clear': 'Predominantemente limpo',
+    'Mostly cloudy': 'Predominantemente nublado',
+    'Partly sunny': 'Parcialmente ensolarado',
+    'Light snow showers': 'Chuvas fracas de neve',
+    'Haze': "Névoa seca"
+};
+
+// Exportar
+module.exports = {
+    locationValidation,
+    removeAccents,
+    translatedConditions,
+};
