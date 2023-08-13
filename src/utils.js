7d4e23449a63a4242be49cee7db100656e52769a
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-08-13 20:07:14 -0300

    feat: add removeAccents function on utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,4 +4,9 @@ function locationValidation(location) {
     const checkNumbers = /^\d+$/;
 
     return checkAccentLetters.test(location) || checkNumbers.test(location);
-}
+};
+
+// Função para remover acentos do input do usuário
+function removeAccents(location) {
+    return location.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
