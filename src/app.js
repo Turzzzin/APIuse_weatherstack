74e362d3c89462f1185964d1f3e3b92386586d08
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-08-13 20:59:36 -0300

    feat: complete app.js + starting html.index

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const axios = require('axios');
 require('dotenv').config();
+const express = require('express');
+const routes = require('./src/routes.js');
 
 const server = express();
 const PORT = 3000;
 
 // Configurar o diretório estático (pasta public)
 server.use(express.static('public'));
+
+server.use(routes);
 
 server.listen(PORT, () => {
     console.log(`O servidor está rodando na porta https://localhost:${PORT}`);
