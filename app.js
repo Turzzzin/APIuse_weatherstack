3799bdb121755310babb63614d67b41406993325
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-08-13 19:48:45 -0300

    feat: require all dependencies + static directory

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const server = express();
+const PORT = 3000;
+
+// Configurar o diretório estático (pasta public)
+server.use(express.static('public'));
